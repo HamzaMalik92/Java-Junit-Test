87c3f6730c52fef24baf4b58a57878719387e332
Hamza Malik <hamzamalikcs20@gmail.com>
2026-02-18 13:18:41 +0500

    used BeforeAll/AfterAll

--- src/test/java/shapes/RectangleTest.java
+++ src/test/java/shapes/RectangleTest.java
@@ -1,27 +1,37 @@
 package shapes;
 
 import com.pixcel.shapes.Rectangle;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
 
+    static Rectangle rect; // shared for all tests
+
+    @BeforeAll
+    static void initAll() {
+        System.out.println("RectangleTest.initAll");
+        rect = new Rectangle(5, 5);
+        System.out.println("rect = " + rect);
+    }
+
+    @AfterAll
+    static void cleanupAll() {
+        System.out.println("RectangleTest.cleanupAll");
+        rect = null;
+        System.out.println("rect = " + null);
+    }
+
     @Test
     void shouldDetectSquare() {
-        Rectangle rect = new Rectangle(5.5, 5.5);
         assertTrue(rect.isSquare());
     }
 
     @Test
-    void shouldDetectNonSquare() {
-        Rectangle rect = new Rectangle(5, 3);
-        assertFalse(rect.isSquare());
-    }
-
-    @Test
     void shouldCalculateArea() {
-        Rectangle rect = new Rectangle(5, 3);
-        assertEquals(15, rect.getArea());
+        assertEquals(25, rect.getArea());
     }
 }
